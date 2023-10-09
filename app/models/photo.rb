e1cb3d01f49c9340cfc19b867a9e2153b24404da
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-10-09 21:40:42 +0000

    Photo Scopes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -26,4 +26,7 @@ class Photo < ApplicationRecord
   validates :caption, presence: true
   validates :image, presence: true
   validates :username, presence: true, uniqueness: true
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
